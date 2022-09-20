7c03ae00e27fc6a4451022fee88e065c64856c37
simonebonifazi <simonbonifraz@gmail.com>
2022-09-20 21:01:11 +0200

    added first prototype of db's columns

---
+++ database/migrations/2022_09_20_184623_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');      
+            $table->string('Stazione di partenza');      
+            $table->string('Stazione di arrivo');      
+            $table->timeTz('Orario di partenza');
+            $table->timeTz('Orario di arrivo');
+            $table->tinyInteger('Codice treno');
+            $table->smallInteger('Numero carrozze');
+            $table->boolean('In orario' , 0);
+            $table->boolean('Cancellato' , 1);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
