773654ea597772c5121cf107fb1c543fe13fc38c
simonebonifazi <simonbonifraz@gmail.com>
2022-09-20 20:44:35 +0200

    prepared array

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-    return view('home');
+    public function index()
+    {
+    
+    $trains = [];
+    return view('home', compact('trains'));
 
     }
 }
