1f5efa2bea07345dce1ee432781f5a8a8842ef48
simonebonifazi <simonbonifraz@gmail.com>
2022-09-21 14:09:23 +0200

    added seeder

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Generator as Faker;
 use App\Models\Train;
 use Illuminate\Database\Seeder;
 
@@ -11,21 +12,24 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $new_trains= new Train();
-          
-            $new_trains->azienda = 'rfi';      
-            $new_trains->stazione_di_partenza = '';      
-            $new_trains->stazione_di_arrivo = '';      
-            $new_trains->orario_di_partenza = '';
-            $new_trains->orario_di_arrivo = '';
-            $new_trains->codice_treno = '';
-            $new_trains->numero_carrozze = '';
-            $new_trains->in_orario = '';
-            $new_trains->cancellato = '';
+        for($i =0; $i < 10; $i++){
+
+            $new_trains= new Train();
+            
+            $new_trains->azienda = $faker->company();      
+            $new_trains->stazione_di_partenza = $faker->city();      
+            $new_trains->stazione_di_arrivo = $faker->city();      
+            $new_trains->orario_di_partenza = $faker->time();
+            $new_trains->orario_di_arrivo = $faker->time();
+            $new_trains->codice_treno = $faker->ean8();
+            $new_trains->numero_carrozze = $faker->randomDigit();
+            $new_trains->in_orario = $faker->boolean();
+            $new_trains->cancellato = $faker->boolean();
         
             $new_trains->save();
            
+        }
     }
 }
