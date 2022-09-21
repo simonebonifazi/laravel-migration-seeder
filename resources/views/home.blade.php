6cb0cfbade02bdf67b2e8ecb0c3311eb15c9cca3
simonebonifazi <simonbonifraz@gmail.com>
2022-09-21 14:45:03 +0200

    printed on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,26 @@
 
 @section('main-content')
 
+<div class="text-center">
+    <ul>
+        @foreach($trains as $train)
+        <li class="list-group-item">
+            <div class="card d-flex my-3">
+                <h1> AZIENDA: {{ $train->azienda}} </h1>
+                <h3 class="me-5"> IDENTIFICATIVO TRENO: {{$train->codice_treno}}</h3>
+                <h3 class="me-5"> DA: {{$train->stazione_di_partenza}}</h3>
+                <h3 class="me-5"> A: {{$train->stazione_di_arrivo}}</h3>
+                <h3 class="me-5"> ARRIVO PREVISTO ORE: {{$train->orario_di_partenza}}</h3>
+                <h3 class="me-5"> PARTENZA PREVISTA ORE: {{$train->orario_di_arrivo}}</h3>
+                <h3 class="me-5"> TOTALE CARROZZE TRENO: {{$train->numero_carrozze}}</h3>
+                <h3 class="me-5"> IN ORARIO: {{$train->in_orario}}</h3>
+                <h3> TRENO CANCELLATO: {{$train->cancellato}}</h3>
+            </div>
+        </li>
+
+        @endforeach
+    </ul>
+
+</div>
 
 @endsection
